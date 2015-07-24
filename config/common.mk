897cef3339f2a648c3a7af2b30f3cab6def82229
ROM-Jeremy <Disciple97414@Gmail.com>
2015-07-24 13:06:47 -0400

    Remove Eleven---Add VanillaMusic
    Change-Id: I467077a676ee3d738bf88469b592204d48d7e6d1
    Signed-off-by: ROM-Jeremy <Disciple97414@Gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -70,6 +70,7 @@ PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/FXExplorer.apk:system/app/FXExplorer/FXExplorer.apk \
     vendor/slim/prebuilt/common/OpenCamera.apk:system/app/OpenCamera/OpenCamera.apk \
     vendor/slim/prebuilt/common/KernelAdiutor.apk:system/app/KernelAdiutor/KernelAdiutor.apk \
+    vendor/slim/prebuilt/common/VanillaMusic.apk:system/app/VanillaMusic/VanillaMusic.apk \
     vendor/slim/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
     vendor/slim/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
@@ -87,9 +88,8 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     PhaseBeam
 
-# Eleven
+# AudioFX
 PRODUCT_PACKAGES += \
-    Eleven \
     AudioFX
 
 # CM Hardware Abstraction Framework
